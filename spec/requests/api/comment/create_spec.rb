44071549ec37907e723ab748aeb53ba28aa6b310
maxarvid <max.arvid.anderson@gmail.com>
2021-11-18 11:43:48 +0100

    updates Gemfile.lock, adds create comment spec

---
+++ spec/requests/api/comment/create_spec.rb
@@ -0,0 +1,18 @@
+describe 'POST /api/comments', type: :request do
+  describe 'successfully' do
+    before do
+      post 'api/comments',
+           params: { comment: { body: 'I am the body of the comment' } }
+    end
+
+    it { is_expected.to have_http_status 201 }
+
+    it 'is expected to return a message' do
+      expect(response_json['message']).to eq 'The commment was created'
+    end
+
+    it 'is expected to save the comment in the comments table' do
+      expect(Comment.count).to eq 1
+    end
+  end
+end
