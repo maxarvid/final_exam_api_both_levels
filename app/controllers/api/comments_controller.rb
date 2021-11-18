a2c7ffdfeb8d95fc83855251e09088933d9ab033
maxarvid <max.arvid.anderson@gmail.com>
2021-11-18 13:02:27 +0100

    WIP fixes comments creation with article association

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,13 +1,16 @@
 class Api::CommentsController < ApplicationController
-
   def create
-    Comment.create(body: comment_params)
-    render json: { message: 'The comment was created' }, status: 201
+    if Article.exists?(comment_params[:article_id])
+      Comment.create(body: comment_params[:body], article_id: comment_params[:article_id])
+      render json: { message: 'The comment was created' }, status: 201
+    else
+      render json: { message: 'The comment cannot be empty' }, status: 422
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:article_id, :body)
   end
 end
--- spec/requests/api/comment/create_spec.rb
+++ spec/requests/api/comment/create_spec.rb
@@ -1,14 +1,16 @@
 describe 'POST /api/comments', type: :request do
+  subject { response }
+
   describe 'successfully' do
+    let!(:article) { create(:article) }
     before do
       post '/api/comments',
-           params: { comment: { body: 'I am the body of the comment' } }
+           params: { comment: { article_id: article.id, body: 'I am the body of the comment' } }
     end
 
     it 'is expected to return a 201 response' do
       expect(response).to have_http_status 201
     end
-    
 
     it 'is expected to return a message' do
       expect(response_json['message']).to eq 'The comment was created'
@@ -16,6 +18,21 @@ describe 'POST /api/comments', type: :request do
 
     it 'is expected to save the comment in the comments table' do
       expect(Comment.count).to eq 1
-    end    
+    end
+  end
+
+  describe 'unsuccessfully' do
+    before do
+      post '/api/comment',
+           params: {}
+    end
+
+    it 'is expected to return a 422 response' do
+      expect(resposne).to have_http_status 422
+    end
+
+    it 'is expected to return an error message' do
+      expect(response_json['message']).to eq 'Comment cannot be empty'
+    end
   end
 end
