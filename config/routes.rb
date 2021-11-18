734a2a241c2e3e6803ed1c298f29c19b4beed9e4
maxarvid <max.arvid.anderson@gmail.com>
2021-11-18 11:54:52 +0100

    WIP adds comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'comments/create'
+  end
   mount_devise_token_auth_for 'User', at: 'auth/api'
   namespace :api do
     resources :articles, only: %i[index show]
